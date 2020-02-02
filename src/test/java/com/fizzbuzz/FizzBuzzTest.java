7cea07f92b84de9a67bf3d30e788d7c71c04bafd
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-02 12:12:40 +0530

    Added test case for failure of mutiples of 3

--- src/test/java/com/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -11,4 +11,10 @@ public class FizzBuzzTest {
 		Assert.assertEquals("1", fizzBuzzConverter.converter(1));
 		Assert.assertEquals("2", fizzBuzzConverter.converter(2));
 	}
+	
+	@Test
+	public void fizzBuzzConvertorLeavesMultipleOfThree() {
+		FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
+		Assert.assertEquals("Fizz", fizzBuzzConverter.converter(3));
+	}
 }
