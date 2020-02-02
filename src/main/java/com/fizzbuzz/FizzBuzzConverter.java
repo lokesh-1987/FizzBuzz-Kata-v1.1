e0079f327f777629e0915654e535bac18313268d
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-02 12:23:32 +0530

    Added test case for success of mutiples of 3 and 5

--- src/main/java/com/fizzbuzz/FizzBuzzConverter.java
+++ src/main/java/com/fizzbuzz/FizzBuzzConverter.java
@@ -3,7 +3,9 @@ package com.fizzbuzz;
 public class FizzBuzzConverter {
 
 	public String converter(int toFizzBuzz) {
-		if (toFizzBuzz % 3 == 0) {
+		if (toFizzBuzz % (3 * 5) == 0) {
+			return "FizzBuzz";
+		} else if (toFizzBuzz % 3 == 0) {
 			return "Fizz";
 		} else if(toFizzBuzz % 5 == 0) {
 			return "Buzz";
