34cdf5dc26b6a68cd260d756b43b862df316d772
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-02 12:31:10 +0530

    removed  FizzBuzzConverter from each method and created a class instance

--- src/test/java/com/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -5,36 +5,33 @@ import org.junit.Test;
 
 public class FizzBuzzTest {
 
+	FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
+	
 	@Test
 	public void fizzBuzzConvertorLeavesNormalNumbersAlone() {
-		FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
 		Assert.assertEquals("1", fizzBuzzConverter.converter(1));
 		Assert.assertEquals("2", fizzBuzzConverter.converter(2));
 	}
 	
 	@Test
 	public void fizzBuzzConvertorLeavesMultipleOfThree() {
-		FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
 		Assert.assertEquals("Fizz", fizzBuzzConverter.converter(3));
 		Assert.assertEquals("Fizz", fizzBuzzConverter.converter(33));
 	}
 	
 	@Test
 	public void fizzBuzzConvertorLeavesMultipleOfFive() {
-		FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
 		Assert.assertEquals("Buzz", fizzBuzzConverter.converter(5));
 		Assert.assertEquals("Buzz", fizzBuzzConverter.converter(65));
 	}
 	
 	@Test
 	public void fizzBuzzConvertorLeavesMultipleOfThreeAndFive() {
-		FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
 		Assert.assertEquals("FizzBuzz", fizzBuzzConverter.converter(60));
 	}
 	
 	@Test 
 	public void printTheHundredFizzBuzzes() { 
-	  FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter(); 
 	  for(int i=1; i<=100; i++){ 
 	    System.out.println(fizzBuzzConverter.converter(i)); 
 	  } 
